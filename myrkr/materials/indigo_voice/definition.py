cc23a8ad0e2d2ed45b41481cc30d8fd583170fb8
Trevor Bača <trevorbaca@gmail.com>
2015-09-16 10:14:44 -0400

    Reworked indigo voice.

--- myrkr/materials/indigo_voice/definition.py
+++ myrkr/materials/indigo_voice/definition.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 from abjad import *
 
 
@@ -10,13 +11,39 @@ tuplet_ratios = sequencetools.partition_sequence_by_counts(
     cyclic=True,
     overhang=True,
     )
-#print(len(tuplet_ratios))
 #print(tuplet_ratios)
+#print(list(sum(_) for _ in tuplet_ratios))
+prolation_indicators = [0, 0]
+assert all(_ in (-1, 0, 1) for _ in prolation_indicators)
+prolation_indicators = datastructuretools.CyclicTuple(prolation_indicators)
 
-maker = rhythmmakertools.TupletRhythmMaker(
-    tuplet_ratios=tuplet_ratios,
-    )
-divisions = 8 * [(1, 4), (2, 4)]
-music = maker(divisions)
-indigo_voice = Voice(music)
-#print(indigo_voice)
+denominator = 16
+tuplets = []
+for i, tuplet_ratio in enumerate(tuplet_ratios):
+    assert mathtools.all_are_positive_integers(tuplet_ratio)
+    weight = sum(tuplet_ratio)    
+    prolation_indicator = prolation_indicators[i]
+    if prolation_indicator == -1:
+        scaled_weight = 2 ** math.floor(math.log(weight, 2))
+    elif prolation_indicator == 0:
+        scaled_weight = weight
+    elif prolation_indicator == 1:
+        scaled_weight = 2 ** math.ceil(math.log(weight, 2))
+    else:
+        raise ValueError(prolation_indicator)
+    notes = []
+    for term in tuplet_ratio:
+        duration = Duration(term, denominator)
+        note = Note("c'", duration)
+        notes.append(note)
+    duration = Duration(scaled_weight, denominator)
+    tuplet = scoretools.FixedDurationTuplet(duration, notes)
+    beam = spannertools.MultipartBeam()
+    attach(beam, tuplet)
+    tuplets.append(tuplet)
+
+time_signatures = []
+for tuplet in tuplets:
+    duration = inspect_(tuplet).get_duration()
+    nonreduced_fraction = duration.with_denominator(denominator)
+    time_signatures.append(nonreduced_fraction)
