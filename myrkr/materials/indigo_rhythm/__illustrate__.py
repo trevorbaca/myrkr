ad0384f05a89261ce3e984cf54acd800df463937
Trevor Bača <trevorbaca@gmail.com>
2015-09-16 15:25:56 -0400

    Reworked indigo rhythm.

--- myrkr/materials/indigo_rhythm/__illustrate__.py
+++ myrkr/materials/indigo_rhythm/__illustrate__.py
@@ -12,13 +12,15 @@ def make_lilypond_file(tuplets, time_signatures):
         tuplets,
         time_signatures,
         )
+    score = lilypond_file.score_block.items[0]
+    assert inspect_(score).is_well_formed()
+    score.add_final_bar_line()
     lilypond_file.layout_block.indent = 0
     lilypond_file.header_block.subtitle = Markup('(Myrkr)')
     lilypond_file.header_block.tagline = markuptools.Markup.null()
     lilypond_file.header_block.title = Markup('Indigo rhythm')
     vector = layouttools.make_spacing_vector(0, 20, 0, 0)
     lilypond_file.paper_block.markup_system_spacing = vector
-    lilypond_file.score_block.items[0].add_final_bar_line()
     return lilypond_file
 
 if __name__ == '__main__':
