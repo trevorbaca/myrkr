65c455ad06c025cc9482cace0145f3ab4cfdfc12
Trevor Bača <trevorbaca@gmail.com>
2015-09-21 15:42:16 -0400

    Built ochre rhythm.

---
+++ myrkr/materials/ochre_rhythm/definition.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from abjad import *
+import myrkr
+
+
+maker = myrkr.makers.RhythmMaker(
+    terms=reversed((1, 2, 3, 2, 3, 1, 3, 2, 2, 3, 1, 2, 3, 2)),
+    counts=(5, 4),
+    denominator=8,
+    prolation_indicators=(0, 0, -1, -1),
+    split_indicators=(0, 0, 1, 1),
+    displace_split_tuplets=True,
+    )
+tuplets = maker()
+time_signatures = maker.make_time_signatures(tuplets)
