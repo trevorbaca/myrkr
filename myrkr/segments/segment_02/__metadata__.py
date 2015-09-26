dd46e671ac1c644116c717e7807ff885becfc603
Trevor Bača <trevorbaca@gmail.com>
2015-09-26 17:38:52 -0500

    Reworked segment-maker.

--- myrkr/segments/segment_02/__metadata__.py
+++ myrkr/segments/segment_02/__metadata__.py
@@ -15,10 +15,12 @@ metadata = datastructuretools.TypedOrderedDict(
         (
             'end_instruments_by_staff',
             datastructuretools.TypedOrderedDict(
-                []
+                [
+                    ('Clarinet Music Staff', 'bass clarinet'),
+                    ]
                 ),
             ),
-        ('end_tempo', 96),
+        ('end_tempo', 110),
         ('end_time_signature', '5/11'),
         ('first_bar_number', 3),
         ('measure_count', 20),
