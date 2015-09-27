696492722bdb175482de1748497b6e7e47bd591e
Trevor Bača <trevorbaca@gmail.com>
2015-09-27 11:12:20 -0500

    Updated tests.

--- myrkr/test/test_segments.py
+++ myrkr/test/test_segments.py
@@ -9,26 +9,17 @@ abjad_ide = ide.tools.idetools.AbjadIDE()
 
 
 if __name__ == '__main__':
-    this_file = os.path.abspath(__file__)
-    test_directory = os.path.dirname(this_file)
-    inner_score_directory = os.path.dirname(test_directory)
-    segments_directory = os.path.join(inner_score_directory, 'segments')
-    # By-hand management here is a Travis workaround:
-    # Travis ide.cfg is not configured;
-    # when Travis ide.cfg is configured this can be simplified to
-    # segments_directories = abjad_ide._list_visible_paths(segments_directory)
-    segment_directories = []
-    for entry in os.listdir(segments_directory):
-        if not entry[0].isalpha():
-            continue
-        segment_directory = os.path.join(segments_directory, entry)
-        segment_directories.append(segment_directory)
-    for segment_directory in segment_directories:
-        message = 'Making {} PDF ...'
-        message = message.format(abjad_ide._trim_path(segment_directory))
-        print(message)
-        try:
-            abjad_ide.make_pdf(segment_directory)
-        except:
-            traceback.print_exc()
-            sys.exit(1)
+#    this_file = os.path.abspath(__file__)
+#    segments_directory = os.path.join(this_file, 'segments')
+#    segment_directories = abjad_ide._list_visible_paths(segments_directory)
+#    for segment_directory in segment_directories:
+#        message = 'Making {} PDF ...'
+#        message = message.format(abjad_ide._trim_path(segment_directory))
+#        print(message)
+#        try:
+#            abjad_ide.make_pdf(segment_directory)
+#        except:
+#            traceback.print_exc()
+#            sys.exit(1)
+    print(abjad_ide._configuration)
+    print(abjad_ide._configuration.composer_scores_directory)
