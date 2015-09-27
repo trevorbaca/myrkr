431ee74f9eaf115df081c78b29b6d0be9ad650a6
Trevor Bača <trevorbaca@gmail.com>
2015-09-27 11:39:58 -0500

    Updated tests.

--- myrkr/test/test_segments.py
+++ myrkr/test/test_segments.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import os
-import ide
+import pytest
 import sys
 import traceback
+import ide
 from abjad.tools import systemtools
 abjad_ide = ide.tools.idetools.AbjadIDE()
 
 
 if __name__ == '__main__':
-    print(abjad_ide._configuration)
-    print(abjad_ide._configuration.composer_scores_directory)
     this_file = os.path.abspath(__file__)
     test_directory = os.path.dirname(this_file)
     inner_score_directory = os.path.dirname(test_directory)
@@ -19,11 +18,19 @@ if __name__ == '__main__':
     # Travis monkey patch
     abjad_ide._configuration._composer_scores_directory_override = \
         composer_scores_directory
-    print(abjad_ide._configuration.composer_scores_directory)
     segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
     segment_directories = abjad_ide._list_visible_paths(segments_directory)
-    print(repr(segments_directory))
-    print(repr(segment_directories))
+
+    for segment_directory in segment_directories:
+        message = 'Checking {} definition file ...'
+        message = message.format(abjad_ide._trim_path(segment_directory))
+        print(message)
+        try:
+            abjad_ide.check_definition_file(segment_directory)
+        except:
+            traceback.print_exc()
+            sys.exit(1)
+
     for segment_directory in segment_directories:
         message = 'Making {} PDF ...'
         message = message.format(abjad_ide._trim_path(segment_directory))
