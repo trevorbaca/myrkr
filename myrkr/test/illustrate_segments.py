d23a0c6e637e3ee0f9d1f3544b7861bb56b958f2
Trevor Bača <trevorbaca@gmail.com>
2015-09-27 09:17:58 -0500

    Updated tests.

--- myrkr/test/illustrate_segments.py
+++ myrkr/test/illustrate_segments.py
@@ -9,6 +9,7 @@ abjad_ide = ide.tools.idetools.AbjadIDE()
 if __name__ == '__main__':
     this_file = os.path.abspath(__file__)
     segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
+    print(repr(segments_directory))
     paths = abjad_ide._list_visible_paths(segments_directory)
     for path in paths:
         message = 'making {} PDF ...'
