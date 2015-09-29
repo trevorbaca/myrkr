0ac5cd9d8bc37ba0efc956f0d228d2a93c385df6
Trevor Bača <trevorbaca@gmail.com>
2015-09-29 01:06:53 -0500

    Reworked [5].

--- myrkr/segments/segment_05/definition.py
+++ myrkr/segments/segment_05/definition.py
@@ -16,26 +16,26 @@ emerald_position = 25
 indigo_position = 80
 preprocessor = myrkr.makers.Preprocessor(
     indicators=(
-        ('indigo', (3, indigo_position)), # 1
-        ('cobalt', (1, cobalt_position)), # 2
-        ('cobalt', 1), # 3
-        ('indigo', 2), # 4
+        ('indigo', (3, indigo_position), 'A3', 'p', ('C', 44)), # 1
+        ('cobalt', (1, cobalt_position), 'Eb2', 'mp'), # 2
+        ('cobalt', 1, 'E2', 'mp'), # 3
+        ('indigo', 2, 'A3', 'mp', ('C', 46)), # 4
 
-        ('indigo', (3, indigo_position)), # 5
-        ('cobalt', (1, cobalt_position)), # 6
-        ('cobalt', 1), # 7
-        ('indigo', 2), # 8
+        ('indigo', (3, indigo_position), 'B3', 'mf', ('C', 48)), # 5
+        ('cobalt', (1, cobalt_position), 'E2', 'mp'), # 6
+        ('cobalt', 1, 'Eb2', 'mp'), # 7
+        ('indigo', 2, 'B3', 'f', ('C', 50)), # 8
 
-        ('indigo', (3, indigo_position)), # 9
-        ('cobalt', (1, cobalt_position)), # 10
-        ('cobalt', 1), # 11
-        ('indigo', 2), # 12
+        ('indigo', (3, indigo_position), 'C4', 'ff', ('C', 52)), # 9
+        ('cobalt', (1, cobalt_position), 'Eb2', 'mp'), # 10
+        ('cobalt', 1, 'E2', 'mp'), # 11
+        ('indigo', 2, 'C4', 'fff', ('C', 54)), # 12
 
-        ('indigo', (3, indigo_position)), # 13
-        ('cobalt', (1, cobalt_position)), # 14
-        ('cobalt', 1), # 15
-        ('charcoal', (3, charcoal_position)), # 16
-        ('indigo', 6), # 17
+        ('indigo', (3, indigo_position), 'C#4', 'ffff', ('C', 56)), # 13
+        ('cobalt', (1, cobalt_position), 'E2', 'mp'), # 14
+        ('cobalt', 1, 'Eb2', 'mp'), # 15
+        ('charcoal', (3, charcoal_position), 'D5 Eb5 D5 C#5', 'pppp'), # 16
+        ('indigo', 6, 'C#4', 'ffff', ('C', 58)), # 17
     ),
     name_to_rhythm={
         'charcoal': myrkr.materials.charcoal_rhythm,
@@ -47,6 +47,9 @@ preprocessor = myrkr.makers.Preprocessor(
         'white': myrkr.materials.white_rhythm,
         },
     )
+# Charcoal position: 3 ...
+# Cobalt position: 6 ...
+# Indigo position: 89 ...
 
 
 ###############################################################################
@@ -59,6 +62,17 @@ segment_maker = myrkr.makers.SegmentMaker(
     show_stage_annotations=True,
     spacing_map=(
         (1, Duration(1, 24)),
+        (2, Duration(1, 8)),
+        (4, Duration(1, 24)),
+
+        (6, Duration(1, 8)),
+        (8, Duration(1, 24)),
+
+        (10, Duration(1, 8)),
+        (12, Duration(1, 24)),
+
+        (14, Duration(1, 8)),
+        (17, Duration(1, 24)),
         ),
     tempo_map=(
         (1, myrkr.materials.tempi[110]),
@@ -96,5 +110,17 @@ segment_maker.make_music_handler(
     scope=(cl, (1, segment_maker.stage_count)),
     specifiers=[
         dynamic_line_spanner_staff_padding(4),
+        ],
+    )
+
+segment_maker.make_music_handler(
+    scope=(cl, 16),
+    specifiers=[
+        baca.makers.GlissandoSpecifier(
+            patterns=[
+                rhythmmakertools.select_all(),
+                rhythmmakertools.silence_last(1),
+                ],
+            ),
         ],
     )
