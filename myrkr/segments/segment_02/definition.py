92f97c55378cbd280877208db279878e8ae7b8ce
Trevor Bača <trevorbaca@gmail.com>
2015-09-28 23:48:15 -0500

    Reworked [2].

--- myrkr/segments/segment_02/definition.py
+++ myrkr/segments/segment_02/definition.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from abjad import *
-import baca
 import myrkr
 from baca.__abbreviations__ import *
 from myrkr.materials.__abbreviations__ import *
@@ -15,27 +14,24 @@ preprocessor = myrkr.makers.Preprocessor(
         ('indigo', (12, 16), 'Eb4', 'ppp', ('C', 0)), # 1
         ('emerald', 1, 'C4', 'mf', ('A', 0)), # 2
         ('indigo', 4, 'Eb4', 'ppp', ('C', 2)), # 3
-        ('emerald', 1, 'C4', 'mf'), # 4
+        ('emerald', 1, 'C4', 'mf', ('A', 2)), # 4
         ('indigo', 1, 'Eb4', 'ppp', ('C', 4)), # 5
-        ('indigo', 3, 'E4', 'ppp', ('C', 3)), # 6
-        ('emerald', 1, 'C4', 'f'), # 7
-        ('indigo', 2, 'E4', 'ppp', ('C', 6)), # 8
-        ('emerald', 1, 'C4 Bb3', 'f'), # 9
-        ('indigo', 4, 'E4', 'ppp', ('C', 8)), # 10
-        ('emerald', 1, 'Bb3', 'f'), # 11
-        ('indigo', 2, 'E4', 'ppp', ('C', 10)), # 12
-        ('emerald', 1, 'Bb3', 'f'), # 13
-        ('indigo', 2, 'E4', 'ppp', ('C', 12)), # 14
-        ('indigo', 2, 'F4', 'ppp', ('C', 14)), # 15
-
-        ('emerald', 1, 'Bb3', 'ff'), # 16
-        ('emerald', 2, 'A3', 'ff'), # 17
+        ('indigo', 3, 'E4', 'ppp', ('C', 6)), # 6
+        ('emerald', 1, 'C4', 'f', ('A', 4)), # 7
+        ('indigo', 2, 'E4', 'ppp', ('C', 8)), # 8
+        ('emerald', 1, 'C4 Bb3', 'f', ('A', 6)), # 9
+        ('indigo', 4, 'E4', 'ppp', ('C', 10)), # 10
+        ('emerald', 1, 'Bb3', 'f', ('A', 8)), # 11
+        ('indigo', 2, 'E4', 'ppp', ('C', 12)), # 12
+        ('emerald', 1, 'Bb3', 'f', ('A', 10)), # 13
+        ('indigo', 2, 'E4', 'ppp', ('C', 14)), # 14
+        ('indigo', 2, 'F4', 'ppp', ('C', 16)), # 15
+        ('emerald', 1, 'Bb3', 'ff', ('A', 12)), # 16
+        ('emerald', 2, 'A3', 'ff', ('A', 14)), # 17
         ('indigo', 2, 'F4', 'ppp', ('C', 16)), # 18
-        ('emerald', 4, 'Ab3', 'fff'), # 19
-
-        ('indigo', 2, 'F4', 'ppp'), # 20
-        ('indigo', 6, 'F#4', 'pppp'), # 21
-
+        ('emerald', 4, 'Ab3', 'fff', ('A', 16)), # 19
+        ('indigo', 2, 'F4', 'ppp', ('C', 18)), # 20
+        ('indigo', 6, 'F#4', 'pppp', ('C', 20)), # 21
         ),
     name_to_rhythm={
         'charcoal': myrkr.materials.charcoal_rhythm,
