f910a4bf1266e796e361710e541993b085d22149
Trevor Bača <trevorbaca@gmail.com>
2015-12-04 10:31:12 -0500

    Updated metadata.

--- myrkr/__metadata__.py
+++ myrkr/__metadata__.py
@@ -3,7 +3,7 @@ from abjad import *
 
 
 metadata = datastructuretools.TypedOrderedDict([
-    ('catalog_number', 'AWN-016'),
+    ('catalog_number', 'AWN-015'),
     ('composer', 'Ba\xc4\x8da'),
     ('forces_tagline', 'for bass clarinet'),
     ('paper_dimensions', '17 x 11 in'),
