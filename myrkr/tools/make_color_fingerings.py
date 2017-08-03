cbe426d54f530dea1bba91b8265ca0ea3e4ccf92
Trevor Bača <trevor.baca@gmail.com>
2017-08-03 12:29:34 -0500

    Integrated namespace updates.

--- myrkr/tools/make_color_fingerings.py
+++ myrkr/tools/make_color_fingerings.py
@@ -8,7 +8,7 @@ def make_color_fingerings(name, index=0):
     r'''Makes color fingerings.
     '''
     color_fingerings = myrkr.materials.color_fingerings[name]
-    color_fingerings = baca.Sequence(color_fingerings).rotate(n=index)
+    color_fingerings = baca.sequence(color_fingerings).rotate(n=index)
     color_fingerings = baca.ColorFingeringCommand(
         deposit_annotations=['color fingering'],
         number_lists=(
