31bcf0bb788c2f8bf13d0af92706fba7dcd9f558
Trevor Bača <trevor.baca@gmail.com>
2017-08-08 14:57:13 -0700

    Updated instrument manifest.

--- myrkr/materials/instruments/definition.py
+++ myrkr/materials/instruments/definition.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
 import abjad
+import baca
 
-
-long_space = 16
-short_space = 10
-
-def make_markup(string, space):
-    string_parts = string.split()
-    if len(string_parts) == 1:
-        markup = abjad.Markup(string_parts[0]).hcenter_in(space)
-    else:
-        markups = [abjad.Markup(_) for _ in string_parts]
-        markup = abjad.Markup.center_column(markups, direction=None)
-        markup = markup.hcenter_in(space)
-    return markup
-
-def make_long_markup(string):
-    return make_markup(string, long_space)
-
-def make_short_markup(string):
-    return make_markup(string, short_space)
 
 instruments = abjad.TypedOrderedDict([
     (
         'bass clarinet', 
         abjad.instrumenttools.BassClarinet(
-            instrument_name_markup=make_long_markup('Bass clarinet'),
-            short_instrument_name_markup=make_short_markup('Bass cl.'),
+            instrument_name_markup=\
+                baca.markup.instrument_name('Bass clarinet'),
+            short_instrument_name_markup=\
+                baca.markup.short_instrument_name('Bass cl.'),
             )
         ),
     ])
