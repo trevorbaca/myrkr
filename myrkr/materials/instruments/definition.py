c8a60d062e937ebbc6eadeb040ffd9dc29cf9eb7
Trevor Bača <trevor.baca@gmail.com>
2017-08-11 09:18:47 -0700

    Integrated instrument name updates.

--- myrkr/materials/instruments/definition.py
+++ myrkr/materials/instruments/definition.py
@@ -7,10 +7,10 @@ instruments = abjad.TypedOrderedDict([
     (
         'bass clarinet', 
         abjad.instrumenttools.BassClarinet(
-            instrument_name_markup=\
-                baca.markup.instrument_name('Bass clarinet'),
-            short_instrument_name_markup=\
-                baca.markup.short_instrument_name('Bass cl.'),
+            name_markup=\
+                baca.markup.instrument('Bass clarinet'),
+            short_name_markup=\
+                baca.markup.short_instrument('Bass cl.'),
             )
         ),
     ])
