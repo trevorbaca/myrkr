d74929e9a7c6bc207802c9cd681c19518f26ab16
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:32:05 -0500

    Cleaned up metadat.

--- myrkr/__metadata__.py
+++ myrkr/__metadata__.py
@@ -3,10 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict([
     ('catalog_number', 'AWN-012'),
-    ('composer', 'Bača'),
+    ('composer', 'TrevorBača'),
     ('forces_tagline', 'for bass clarinet'),
-    ('paper_size', '17 x 11 in'),
-    ('price', '\\$28 / \\euro 22'),
     ('title', 'Myrkr'),
     ('year', 2015),
     ])
