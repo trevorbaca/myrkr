63a301e8062c0e94f2f021cd2b55fe99fb5e33ca
Trevor Bača <trevor.baca@gmail.com>
2017-09-29 10:05:12 -0500

    Cleaned up instruments.

--- myrkr/materials/instruments/definition.py
+++ myrkr/materials/instruments/definition.py
@@ -5,7 +5,7 @@ import baca
 instruments = abjad.InstrumentDictionary([
     (
         'bass clarinet',
-        abjad.instrumenttools.BassClarinet(
+        abjad.BassClarinet(
             name_markup=baca.markup.instrument('Bass clarinet'),
             short_name_markup=baca.markup.short_instrument('Bass cl.'),
             )
