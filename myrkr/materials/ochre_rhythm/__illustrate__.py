b41f9bf54676efabb8bd092c017329757e93bc1b
Trevor Bača <trevor.baca@gmail.com>
2017-09-30 12:21:53 -0500

    Removed illustrate files.

--- myrkr/materials/ochre_rhythm/__illustrate__.py
+++
@@ -1,8 +0,0 @@
-from myrkr.materials.ochre_rhythm.definition import maker
-
-
-lilypond_file = maker.__illustrate__(
-    title='Ochre rhythm',
-    subtitle='(Myrkr)',
-    proportional_notation_duration=(1, 12),
-    )
--- myrkr/materials/white_rhythm/__illustrate__.py
+++
@@ -1,8 +0,0 @@
-from myrkr.materials.white_rhythm.definition import maker
-
-
-lilypond_file = maker.__illustrate__(
-    title='White rhythm',
-    subtitle='(Myrkr)',
-    proportional_notation_duration=(1, 24),
-    )
