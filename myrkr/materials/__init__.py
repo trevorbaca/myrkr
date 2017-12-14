487b32496fccae14777bfaacc5a521313a9f5980
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 16:01:14 -0600

    Cleaned up initializers.

--- myrkr/materials/__init__.py
+++ myrkr/materials/__init__.py
@@ -1,7 +1,9 @@
-import abjad
-
-
-abjad.ImportManager.import_material_packages(
-    __path__[0],
-    globals(),
-    )
+from .charcoal_rhythm.definition import charcoal_rhythm
+from .cobalt_rhythm.definition import cobalt_rhythm
+from .emerald_rhythm.definition import emerald_rhythm
+from .graphite_rhythm.definition import graphite_rhythm
+from .indigo_rhythm.definition import indigo_rhythm
+from .instruments.definition import instruments
+from .metronome_marks.definition import metronome_marks
+from .ochre_rhythm.definition import ochre_rhythm
+from .white_rhythm.definition import white_rhythm
