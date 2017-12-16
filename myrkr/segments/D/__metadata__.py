c007fc35bf433b1579d816769ca0a15f80aea921
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 11:23:40 -0600

    Cleaning up metadata.

--- myrkr/segments/D/__metadata__.py
+++ myrkr/segments/D/__metadata__.py
@@ -31,13 +31,9 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        ('end_margin_markup', None),
         ('end_metronome_mark', '55'),
-        (
-            'end_staff_lines',
-            abjad.TypedOrderedDict(
-                []
-                ),
-            ),
+        ('end_staff_lines', None),
         ('end_time_signature', '6/22'),
         ('first_bar_number', 111),
         ('measure_count', 31),
