decaaf742bd5620bdfed675b7e4166ad624a208c
Trevor Bača <trevor.baca@gmail.com>
2017-12-21 10:21:55 -0600

    Cleaning up instrument tags.

--- myrkr/materials/instruments/definition.py
+++ myrkr/materials/instruments/definition.py
@@ -6,8 +6,8 @@ instruments = abjad.InstrumentDictionary([
     (
         'BassClarinet',
         abjad.BassClarinet(
-            name_markup=baca.markup.instrument('Bass clarinet'),
-            short_name_markup=baca.markup.short_instrument('Bass cl.'),
+            markup=baca.markup.instrument('Bass clarinet'),
+            short_markup=baca.markup.short_instrument('Bass cl.'),
             )
         ),
     ])
