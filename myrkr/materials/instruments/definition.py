fc567e21f1597dfcc7673269e88c93735dc7ff2b
Trevor Bača <trevor.baca@gmail.com>
2018-03-30 11:34:26 -0500

    Cleaned up instrument dictionary.

--- myrkr/materials/instruments/definition.py
+++ myrkr/materials/instruments/definition.py
@@ -5,10 +5,6 @@ import baca
 instruments = abjad.OrderedDict([
     (
         'BassClarinet',
-        abjad.BassClarinet(
-            hide=True,
-            markup=baca.markup.instrument('Bass clarinet'),
-            short_markup=baca.markup.short_instrument('Bass cl.'),
-            )
+        abjad.BassClarinet(hide=True),
         ),
     ])
