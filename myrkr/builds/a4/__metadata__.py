8feee51c9529f1e32fe4c35475640a98fbb1143b
Trevor Bača <trevor.baca@gmail.com>
2019-01-10 17:00:03 -0600

    Rerendered covers.

--- myrkr/builds/a4/__metadata__.py
+++ myrkr/builds/a4/__metadata__.py
@@ -3,8 +3,8 @@ import abjad
 
 metadata = abjad.OrderedDict(
     [
-        ('catalog_number_suffix', 'AWN-012 (A4)'),
+        ('catalog_number_suffix', 'A4'),
         ('paper_size', 'a4'),
-        ('price', '\\$28 / \\euro 22'),
+        ('price', '\\$28 / \\euro 28'),
         ]
     )
