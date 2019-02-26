162a57697475a0672f643ddbe37e13bfd6a4b3f6
Trevor Bača <trevor.baca@gmail.com>
2019-02-26 10:20:17 -0600

    Cleaned up persist files.

--- myrkr/segments/M/__metadata__.py
+++ myrkr/segments/M/__metadata__.py
@@ -3,17 +3,6 @@ import abjad
 
 metadata = abjad.OrderedDict(
     [
-        (
-            'alive_during_segment',
-            [
-                'Score',
-                'Global_Context',
-                'Global_Skips',
-                'Music_Context',
-                'Clarinet_Music_Staff',
-                'Clarinet_Music_Voice',
-                ],
-            ),
         ('duration', "1'38''"),
         ('final_measure_number', 383),
         ('first_measure_number', 364),
