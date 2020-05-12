dc7ecdae80eeeb8f1e33ef7a8ef5465ae073a168
Trevor Bača <trevor.baca@gmail.com>
2020-05-12 10:07:25 -0400

    Replaced tools/ directory with tools.py module.

--- myrkr/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ColorMaker import ColorMaker
-from .Preprocessor import Preprocessor
-from .RhythmMaker import RhythmMaker
-from .ScoreTemplate import ScoreTemplate
-from .color_fingerings import color_fingerings
