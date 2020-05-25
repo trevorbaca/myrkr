a47c3c58f057e0c6a38993fcd41d03817c5c02e8
Trevor Bača <trevor.baca@gmail.com>
2020-05-25 12:38:36 -0400

    Replaced materials/ directory with materials.py module.

--- myrkr/materials/charcoal_rhythm/definition.py
+++
@@ -1,9 +0,0 @@
-from myrkr.tools import RhythmMaker
-
-maker = RhythmMaker(
-    terms=(2, 2, 3, 2, 2, 3, 2, 3),
-    counts=(2, 3),
-    denominator=4,
-    prolation_indicators=(0, -1, -1),
-)
-charcoal_rhythm = maker()
--- myrkr/materials/graphite_rhythm/definition.py
+++
@@ -1,4 +0,0 @@
-from myrkr.tools import RhythmMaker
-
-maker = RhythmMaker(terms=(1,), counts=(1,), denominator=4)
-graphite_rhythm = maker()
