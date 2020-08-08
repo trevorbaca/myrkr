60fc7b378f961b48c4eddff71ef8848ad172b002
Trevor Bača <trevor.baca@gmail.com>
2020-08-08 16:00:22 -0400

    Cleaned up tags.

--- myrkr/segments/_/definition.py
+++ myrkr/segments/_/definition.py
@@ -24,7 +24,7 @@ preprocessor = myrkr.Preprocessor(
 
 maker = baca.SegmentMaker(
     activate=[
-        abjad.tags.LOCAL_MEASURE_NUMBER,
+        baca.tags.LOCAL_MEASURE_NUMBER,
     ],
     check_all_are_pitched=True,
     segment_directory=abjad.Path(__file__).resolve().parent,
