a2824f4ff948b7d9aaae6e7647b74593496c2132
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:08:28 -0400

    Blacked code.

--- myrkr/segments/E/__persist__.py
+++ myrkr/segments/E/__persist__.py
@@ -5,64 +5,64 @@ import ide
 persist = abjad.OrderedDict(
     [
         (
-            'alive_during_segment',
+            "alive_during_segment",
             [
-                'Score',
-                'Global_Context',
-                'Global_Skips',
-                'Music_Context',
-                'Clarinet_Music_Staff',
-                'Global_Rests',
-                'Clarinet_Music_Voice',
-                'Clarinet_Rest_Voice',
-                ],
-            ),
+                "Score",
+                "Global_Context",
+                "Global_Skips",
+                "Music_Context",
+                "Clarinet_Music_Staff",
+                "Global_Rests",
+                "Clarinet_Music_Voice",
+                "Clarinet_Rest_Voice",
+            ],
+        ),
         (
-            'persistent_indicators',
+            "persistent_indicators",
             abjad.OrderedDict(
                 [
                     (
-                        'Clarinet_Music_Staff',
+                        "Clarinet_Music_Staff",
                         [
                             ide.Momento(
-                                context='Clarinet_Music_Voice',
-                                manifest='instruments',
-                                value='BassClarinet',
-                                ),
+                                context="Clarinet_Music_Voice",
+                                manifest="instruments",
+                                value="BassClarinet",
+                            ),
                             ide.Momento(
-                                context='Clarinet_Music_Voice',
-                                prototype='abjad.Clef',
-                                value='treble',
-                                ),
-                            ],
-                        ),
+                                context="Clarinet_Music_Voice",
+                                prototype="abjad.Clef",
+                                value="treble",
+                            ),
+                        ],
+                    ),
                     (
-                        'Clarinet_Music_Voice',
+                        "Clarinet_Music_Voice",
                         [
                             ide.Momento(
-                                context='Clarinet_Music_Voice',
-                                prototype='abjad.Dynamic',
-                                value='f',
-                                ),
-                            ],
-                        ),
+                                context="Clarinet_Music_Voice",
+                                prototype="abjad.Dynamic",
+                                value="f",
+                            ),
+                        ],
+                    ),
                     (
-                        'Score',
+                        "Score",
                         [
                             ide.Momento(
-                                context='Global_Skips',
-                                manifest='metronome_marks',
-                                value='110',
-                                ),
+                                context="Global_Skips",
+                                manifest="metronome_marks",
+                                value="110",
+                            ),
                             ide.Momento(
-                                context='Global_Skips',
-                                prototype='abjad.TimeSignature',
-                                value='4/22',
-                                ),
-                            ],
-                        ),
-                    ]
-                ),
+                                context="Global_Skips",
+                                prototype="abjad.TimeSignature",
+                                value="4/22",
+                            ),
+                        ],
+                    ),
+                ]
             ),
-        ]
-    )
+        ),
+    ]
+)
