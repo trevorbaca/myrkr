29eea61dc5207fecd1581ccd7c226a8eec235e26
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:08:28 -0400

    Blacked code.

--- myrkr/segments/_/__metadata__.py
+++ myrkr/segments/_/__metadata__.py
@@ -4,19 +4,19 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [1],
-            ),
-        ('duration', "0'14''"),
-        ('final_measure_number', 1),
-        ('first_measure_number', 1),
-        ('segment_name', '_'),
-        ('segment_number', 1),
-        ('start_clock_time', "0'00''"),
-        ('stop_clock_time', "0'14''"),
+        ),
+        ("duration", "0'14''"),
+        ("final_measure_number", 1),
+        ("first_measure_number", 1),
+        ("segment_name", "_"),
+        ("segment_number", 1),
+        ("start_clock_time", "0'00''"),
+        ("stop_clock_time", "0'14''"),
         (
-            'time_signatures',
-            ['12/5'],
-            ),
-        ]
-    )
+            "time_signatures",
+            ["12/5"],
+        ),
+    ]
+)
