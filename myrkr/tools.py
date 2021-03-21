045886b61b8f38862bc69c0286b8a34827a45618
Trevor Bača <trevor.baca@gmail.com>
2021-03-21 13:54:42 -0400

    Moved score template into tools.py module.

--- myrkr/tools.py
+++ myrkr/tools.py
@@ -702,3 +702,101 @@ ochre_rhythm = maker()
 
 maker = RhythmMaker(terms=(-3, -1, -4, -2), counts=(1,), denominator=16)
 white_rhythm = maker()
+
+
+class ScoreTemplate(baca.ScoreTemplate):
+    """
+    Score template.
+    """
+
+    ### CLASS VARIABLES ###
+
+    _always_make_global_rests = True
+
+    _do_not_require_margin_markup = True
+
+    _global_rests_in_topmost_staff = True
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        super(ScoreTemplate, self).__init__()
+        self.voice_abbreviations.update({"cl": "Clarinet_Music_Voice"})
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self) -> abjad.Score:
+        """
+        Calls score template.
+        """
+        site = "myrkr.ScoreTemplate.__call__()"
+        tag = abjad.Tag(site)
+
+        # GLOBAL CONTEXT
+        global_context = self._make_global_context()
+
+        # CLARINET
+        clarinet_music_voice = abjad.Voice(
+            lilypond_type="ClarinetMusicVoice",
+            name="Clarinet_Music_Voice",
+            tag=tag,
+        )
+        clarinet_music_staff = abjad.Staff(
+            [clarinet_music_voice],
+            lilypond_type="ClarinetMusicStaff",
+            name="Clarinet_Music_Staff",
+            tag=tag,
+        )
+        abjad.annotate(
+            clarinet_music_staff,
+            "default_instrument",
+            instruments["BassClarinet"],
+        )
+        abjad.annotate(clarinet_music_staff, "default_clef", abjad.Clef("treble"))
+
+        # SCORE
+        music_context = abjad.Context(
+            [clarinet_music_staff],
+            lilypond_type="MusicContext",
+            name="Music_Context",
+            tag=tag,
+        )
+        score = abjad.Score([global_context, music_context], name="Score", tag=tag)
+        self._assert_lilypond_identifiers(score)
+        self._assert_unique_context_names(score)
+        self._assert_matching_custom_context_names(score)
+        return score
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def do_not_require_margin_markup(self):
+        """
+        Does not require margin markup.
+
+        ..  container:: example
+
+            >>> myrkr.ScoreTemplate().do_not_require_margin_markup
+            True
+
+        """
+        return super(ScoreTemplate, self).do_not_require_margin_markup
+
+    @property
+    def voice_abbreviations(self):
+        """
+        Gets voice abbreviations.
+
+        ..  container:: example
+
+            >>> score_template = myrkr.ScoreTemplate()
+            >>> string = abjad.storage(score_template.voice_abbreviations)
+            >>> print(string)
+            abjad.OrderedDict(
+                [
+                    ('cl', 'Clarinet_Music_Voice'),
+                    ]
+                )
+
+        """
+        return super(ScoreTemplate, self).voice_abbreviations
