7847bebf58639114f4a0ae288727f7b8b6f2da0b
Trevor Bača <trevor.baca@gmail.com>
2021-08-13 11:16:40 -0400

    Cleaned up layout files.

--- myrkr/segments/02/layout.py
+++ myrkr/segments/02/layout.py
@@ -2,18 +2,18 @@ import baca
 
 distances = (10,)
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=17, y_offset=80, distances=distances),
-        baca.system(measure=32, y_offset=130, distances=distances),
-        baca.system(measure=46, y_offset=180, distances=distances),
+spacing = baca.SpacingSpecifier(
+    breaks=baca.breaks(
+        baca.page(
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=17, y_offset=80, distances=distances),
+            baca.system(measure=32, y_offset=130, distances=distances),
+            baca.system(measure=46, y_offset=180, distances=distances),
+        ),
     ),
+    fallback_duration=(1, 28),
+    overrides=(baca.space(15, (1, 40)),),
 )
 
 if __name__ == "__main__":
-    baca.build.make_layout_ly(
-        breaks,
-        fallback_duration=(1, 28),
-        overrides=(baca.space(15, (1, 40)),),
-    )
+    baca.build.make_layout_ly(spacing)
