353f76e470d1f35de491135bf8aee8b017d5514b
Trevor Bača <trevor.baca@gmail.com>
2021-08-13 11:16:40 -0400

    Cleaned up layout files.

--- myrkr/segments/11/layout.py
+++ myrkr/segments/11/layout.py
@@ -2,13 +2,16 @@ import baca
 
 distances = (10,)
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=21, y_offset=80, distances=distances),
-        baca.system(measure=34, y_offset=130, distances=distances),
+spacing = baca.SpacingSpecifier(
+    breaks=baca.breaks(
+        baca.page(
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=21, y_offset=80, distances=distances),
+            baca.system(measure=34, y_offset=130, distances=distances),
+        ),
     ),
+    fallback_duration=(1, 28),
 )
 
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, fallback_duration=(1, 28))
+    baca.build.make_layout_ly(spacing)
--- myrkr/segments/14/layout.py
+++ myrkr/segments/14/layout.py
@@ -2,14 +2,17 @@ import baca
 
 distances = (10,)
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=11, y_offset=80, distances=distances),
-        baca.system(measure=15, y_offset=130, distances=distances),
-        baca.system(measure=20, y_offset=180, distances=distances),
+spacing = baca.SpacingSpecifier(
+    breaks=baca.breaks(
+        baca.page(
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=11, y_offset=80, distances=distances),
+            baca.system(measure=15, y_offset=130, distances=distances),
+            baca.system(measure=20, y_offset=180, distances=distances),
+        ),
     ),
+    fallback_duration=(1, 28),
 )
 
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, fallback_duration=(1, 28))
+    baca.build.make_layout_ly(spacing)
