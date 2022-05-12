9e6890f3e510e911b0bb7413074ff19d3da3178e
Trevor Bača <trevor.baca@gmail.com>
2022-05-12 16:47:32 +0200

    Cleaned up music.py files.

--- myrkr/sections/02/music.py
+++ myrkr/sections/02/music.py
@@ -78,6 +78,11 @@ commands(
     baca.metronome_mark("110"),
 )
 
+commands(
+    "cl",
+    baca.append_phantom_measure(),
+)
+
 if __name__ == "__main__":
     metadata, persist, score, timing = baca.build.interpret_segment(
         score,
@@ -85,7 +90,9 @@ if __name__ == "__main__":
         **baca.score_interpretation_defaults(),
         activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
         always_make_global_rests=True,
+        append_phantom_measures_by_hand=True,
         do_not_require_margin_markup=True,
+        do_not_sort_commands=True,
         deactivate=(baca.tags.REPEAT_PITCH_CLASS_COLORING,),
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
