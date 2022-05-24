876064844dd2e524859987e16eeb1497577b9d0f
Trevor Bača <trevor.baca@gmail.com>
2022-05-24 21:24:24 +0200

    Cleaned up section identifiers.

--- myrkr/sections/01/music.py
+++ myrkr/sections/01/music.py
@@ -26,7 +26,7 @@ score = library.make_empty_score()
 voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
-    **baca.segment_accumulation_defaults(),
+    **baca.section_accumulation_defaults(),
     instruments=library.instruments,
     metronome_marks=library.metronome_marks,
     time_signatures=preprocessor.time_signatures,
@@ -53,7 +53,7 @@ commands(
 commands(
     "cl",
     baca.append_phantom_measure(),
-    baca.attach_first_segment_default_indicators(),
+    baca.attach_first_section_default_indicators(),
 )
 
 if __name__ == "__main__":
