7e368fc697e089839d3d8bb79d1965c831373cce
Trevor Bača <trevor.baca@gmail.com>
2022-05-29 16:04:25 +0200

    Cleaned up instrument name application.

--- myrkr/sections/01/music.py
+++ myrkr/sections/01/music.py
@@ -53,9 +53,6 @@ commands(
 commands(
     "cl",
     baca.append_phantom_measure(),
-    baca.attach_first_section_default_indicators(
-        attach_instruments_by_hand=True,
-    ),
     baca.instrument(commands.instruments["BassClarinet"]),
     baca.clef("treble"),
 )
