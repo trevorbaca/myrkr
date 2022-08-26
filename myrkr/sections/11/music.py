0f9f485ce91e9a7e6d66124f0e823cc8090d42da
Trevor Bača <trevor.baca@gmail.com>
2022-08-26 09:16:28 -0400

    Cleaned up music.py files

--- myrkr/sections/11/music.py
+++ myrkr/sections/11/music.py
@@ -6,47 +6,48 @@ from myrkr import library
 ########################################### 11 ##########################################
 #########################################################################################
 
-cobalt_position = 27
-indigo_position = 118
-music, time_signatures = library.make_music(
-    # 1-3
-    ("graphite", 9, "C3 Eb3 G3 Bb3 D4 F4 A4 C5 Eb5", "pppp"),
-    ("graphite", 5, "Eb5", "pppp"),
-    ("indigo", (2, indigo_position), "F#3", "f", ("C", 80)),
-    # 4-6
-    ("indigo", 2, "F3", "ff", ("C", 82)),
-    ("graphite", 3, "Eb5", "pppp"),
-    ("indigo", 2, "F3", "ff", ("C", 84)),
-    # 7-9
-    ("cobalt", (1, cobalt_position), "C2", "mp"),
-    ("graphite", 5, "Eb5", "pppp"),
-    ("indigo", 2, "F3", "ff", ("C", 86)),
-    # 10-11
-    ("indigo", 2, "E3", "fff", ("C", 88)),
-    ("cobalt", 1, "C2", "mp"),
-)
-# Cobalt position 29 ...
-# Indigo position 128 ...
 
-score = library.make_empty_score()
-voice_names = baca.accumulator.get_voice_names(score)
-
-accumulator = baca.CommandAccumulator(
-    time_signatures=time_signatures,
-    _voice_abbreviations=library.voice_abbreviations,
-    _voice_names=voice_names,
-)
-
-baca.interpret.set_up_score(
-    score,
-    accumulator.time_signatures,
-    accumulator,
-    library.manifests,
-    append_anchor_skip=True,
-    always_make_global_rests=True,
-)
-
-accumulator.voice("cl").extend(music)
+def make_empty_score(first_measure_number, previous_persistent_indicators):
+    cobalt_position = 27
+    indigo_position = 118
+    music, time_signatures = library.make_music(
+        # 1-3
+        ("graphite", 9, "C3 Eb3 G3 Bb3 D4 F4 A4 C5 Eb5", "pppp"),
+        ("graphite", 5, "Eb5", "pppp"),
+        ("indigo", (2, indigo_position), "F#3", "f", ("C", 80)),
+        # 4-6
+        ("indigo", 2, "F3", "ff", ("C", 82)),
+        ("graphite", 3, "Eb5", "pppp"),
+        ("indigo", 2, "F3", "ff", ("C", 84)),
+        # 7-9
+        ("cobalt", (1, cobalt_position), "C2", "mp"),
+        ("graphite", 5, "Eb5", "pppp"),
+        ("indigo", 2, "F3", "ff", ("C", 86)),
+        # 10-11
+        ("indigo", 2, "E3", "fff", ("C", 88)),
+        ("cobalt", 1, "C2", "mp"),
+    )
+    # Cobalt position 29 ...
+    # Indigo position 128 ...
+    score = library.make_empty_score()
+    voice_names = baca.accumulator.get_voice_names(score)
+    accumulator = baca.CommandAccumulator(
+        time_signatures=time_signatures,
+        _voice_abbreviations=library.voice_abbreviations,
+        _voice_names=voice_names,
+    )
+    baca.interpret.set_up_score(
+        score,
+        accumulator.time_signatures,
+        accumulator,
+        library.manifests,
+        append_anchor_skip=True,
+        always_make_global_rests=True,
+        first_measure_number=first_measure_number,
+        previous_persistent_indicators=previous_persistent_indicators,
+    )
+    accumulator.voice("cl").extend(music)
+    return score, accumulator
 
 
 def postprocess(m):
@@ -57,9 +58,10 @@ def postprocess(m):
         baca.glissando_function(o)
 
 
-def make_score():
-    previous_persist = baca.previous_persist(__file__)
-    previous_persistent_indicators = previous_persist["persistent_indicators"]
+def make_score(first_measure_number, previous_persistent_indicators):
+    score, accumulator = make_empty_score(
+        first_measure_number, previous_persistent_indicators
+    )
     baca.reapply(
         accumulator.voices(),
         library.manifests,
@@ -71,20 +73,27 @@ def make_score():
         library.voice_abbreviations,
     )
     postprocess(cache["cl"])
+    return score, accumulator
 
 
 def main():
-    make_score()
+    previous_metadata = baca.previous_metadata(__file__)
+    first_measure_number = previous_metadata["final_measure_number"] + 1
+    previous_persist = baca.previous_persist(__file__)
+    score, accumulator = make_score(
+        first_measure_number, previous_persist["persistent_indicators"]
+    )
     metadata, persist, timing = baca.build.section(
         score,
         library.manifests,
         accumulator.time_signatures,
         **baca.interpret.section_defaults(),
-        activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
+        activate=[baca.tags.LOCAL_MEASURE_NUMBER],
         always_make_global_rests=True,
-        deactivate=(baca.tags.REPEAT_PITCH_CLASS_COLORING,),
+        deactivate=[baca.tags.REPEAT_PITCH_CLASS_COLORING],
         do_not_require_short_instrument_names=True,
         error_on_not_yet_pitched=True,
+        first_measure_number=first_measure_number,
         global_rests_in_topmost_staff=True,
         transpose_score=True,
     )
