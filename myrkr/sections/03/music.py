f8fbd23e90325e2edca698db193f4a66fa6f4e02
Trevor Bača <trevor.baca@gmail.com>
2022-09-25 18:19:01 -0400

    Cleaned up music.py files.

--- myrkr/sections/03/music.py
+++ myrkr/sections/03/music.py
@@ -65,7 +65,7 @@ def main():
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
     )
-    metadata, persist, timing = baca.build.section(
+    metadata, persist, timing = baca.build.postprocess_score(
         score,
         library.manifests,
         accumulator.time_signatures,
@@ -84,7 +84,7 @@ def main():
         include_layout_ly=True,
         includes=["../stylesheet.ily"],
     )
-    baca.build.persist(
+    baca.build.persist_lilypond_file(
         lilypond_file,
         environment.metadata,
         environment.persist,
