666cae4b94d2569316ef4ab6bd13e2a29659a2b1
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:53 -0400

    Cleaned up metadata.

--- myrkr/sections/06/music.py
+++ myrkr/sections/06/music.py
@@ -107,8 +107,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
