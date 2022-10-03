a91f677419772ef34dc63315090c155794379885
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 09:39:01 -0400

    Cleaned up metadata.

--- myrkr/sections/08/music.py
+++ myrkr/sections/08/music.py
@@ -99,7 +99,7 @@ def main():
         environment.previous_metadata["persistent_indicators"],
         environment.timing,
     )
-    metadata, persist = baca.section.postprocess_score(
+    metadata = baca.section.postprocess_score(
         score,
         measures(),
         **baca.section.section_defaults(),
@@ -121,7 +121,6 @@ def main():
     baca.build.persist_lilypond_file(
         lilypond_file,
         metadata,
-        persist,
         environment.timing,
         environment.arguments,
     )
