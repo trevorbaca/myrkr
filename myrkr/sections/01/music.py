9a560ee0651d5204eba3ddd83980a2e7fdd15bb9
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 09:39:01 -0400

    Cleaned up metadata.

--- myrkr/sections/01/music.py
+++ myrkr/sections/01/music.py
@@ -57,7 +57,7 @@ def make_score():
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
     score, measures = make_score(environment.timing)
-    metadata, persist = baca.section.postprocess_score(
+    metadata = baca.section.postprocess_score(
         score,
         measures(),
         **baca.section.section_defaults(),
@@ -78,7 +78,6 @@ def main():
     baca.build.persist_lilypond_file(
         lilypond_file,
         metadata,
-        persist,
         environment.timing,
         environment.arguments,
     )
--- myrkr/sections/04/music.py
+++ myrkr/sections/04/music.py
@@ -77,7 +77,7 @@ def main():
         environment.previous_metadata["persistent_indicators"],
         environment.timing,
     )
-    metadata, persist = baca.section.postprocess_score(
+    metadata = baca.section.postprocess_score(
         score,
         measures(),
         **baca.section.section_defaults(),
@@ -99,7 +99,6 @@ def main():
     baca.build.persist_lilypond_file(
         lilypond_file,
         metadata,
-        persist,
         environment.timing,
         environment.arguments,
     )
