f47af24b1cec84255eb801567f195d2de6a3380c
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:59 -0400

    Cleaned up layout.py files.

--- myrkr/sections/05/layout.py
+++ myrkr/sections/05/layout.py
@@ -2,15 +2,19 @@ import baca
 
 distances = (10,)
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=13, y_offset=80, distances=distances),
-        baca.system(measure=24, y_offset=130, distances=distances),
-    ),
-    spacing=(1, 28),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=13, y_offset=80, distances=distances),
+            baca.system(measure=24, y_offset=130, distances=distances),
+        ),
+        spacing=(1, 28),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- myrkr/sections/14/layout.py
+++ myrkr/sections/14/layout.py
@@ -2,16 +2,20 @@ import baca
 
 distances = (10,)
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=11, y_offset=80, distances=distances),
-        baca.system(measure=15, y_offset=130, distances=distances),
-        baca.system(measure=20, y_offset=180, distances=distances),
-    ),
-    spacing=(1, 28),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=11, y_offset=80, distances=distances),
+            baca.system(measure=15, y_offset=130, distances=distances),
+            baca.system(measure=20, y_offset=180, distances=distances),
+        ),
+        spacing=(1, 28),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
