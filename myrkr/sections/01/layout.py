0f2e9ed03e1e900f811409f98b199076316c34b0
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:59 -0400

    Cleaned up layout.py files.

--- myrkr/sections/01/layout.py
+++ myrkr/sections/01/layout.py
@@ -2,13 +2,17 @@ import baca
 
 distances = (10,)
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=80, distances=distances),
-    ),
-    spacing=(1, 28),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=80, distances=distances),
+        ),
+        spacing=(1, 28),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- myrkr/sections/03/layout.py
+++ myrkr/sections/03/layout.py
@@ -2,15 +2,19 @@ import baca
 
 distances = (10,)
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=30, distances=distances),
-        baca.system(measure=17, y_offset=80, distances=distances),
-    ),
-    spacing=(1, 28),
-    overrides=(baca.space(8, (1, 40)),),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=17, y_offset=80, distances=distances),
+        ),
+        spacing=(1, 28),
+        overrides=(baca.space(8, (1, 40)),),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
