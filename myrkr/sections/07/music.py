d09d54a0636639e4352c18511ae226cca3be0a65
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:54 -0400

    Cleaned up music.py files.

--- myrkr/sections/07/music.py
+++ myrkr/sections/07/music.py
@@ -111,5 +111,18 @@ def main():
     )
 
 
+def make_layout():
+    distances = (10,)
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30, distances=distances),
+            baca.system(measure=7, y_offset=80, distances=distances),
+        ),
+        spacing=(1, 28),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
