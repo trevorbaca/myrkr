8e20c69943c93996ee80ca268b8041bd902e281b
Trevor Bača <trevor.baca@gmail.com>
2024-03-31 16:21:21 -0400

    Cleaned up music.py files.

--- myrkr/sections/03/music.py
+++ myrkr/sections/03/music.py
@@ -91,7 +91,7 @@ def make_layout():
         default_spacing=(1, 28),
         spacing_overrides=(baca.layout.Override(8, (1, 40)),),
     )
-    baca.section.write_layout_ly(layout)
+    baca.build.write_layout_ly(layout)
 
 
 def main():
