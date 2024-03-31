50bbf2838c374d597ad708e2ba0b31dfdb7b0d9e
Trevor Bača <trevor.baca@gmail.com>
2024-03-31 13:01:52 -0400

    Cleaned up music.py files.

--- myrkr/sections/01/music.py
+++ myrkr/sections/01/music.py
@@ -81,14 +81,14 @@ def persist_score(score, environment):
 
 def make_layout():
     distances = (10,)
-    spacing = baca.make_layout(
+    layout = baca.make_layout(
         baca.page(
             1,
             baca.system(measure=1, y_offset=80, distances=distances),
         ),
         spacing=(1, 28),
     )
-    baca.section.make_layout_ly(spacing)
+    baca.section.make_layout_ly(layout)
 
 
 def main():
--- myrkr/sections/07/music.py
+++ myrkr/sections/07/music.py
@@ -107,7 +107,7 @@ def persist_score(score, environment):
 
 def make_layout():
     distances = (10,)
-    spacing = baca.make_layout(
+    layout = baca.make_layout(
         baca.page(
             1,
             baca.system(measure=1, y_offset=30, distances=distances),
@@ -115,7 +115,7 @@ def make_layout():
         ),
         spacing=(1, 28),
     )
-    baca.section.make_layout_ly(spacing)
+    baca.section.make_layout_ly(layout)
 
 
 def main():
