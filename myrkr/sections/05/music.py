8d2703ab131022d7b7dab630e629271d4b75d6e3
Trevor Bača <trevor.baca@gmail.com>
2024-06-20 08:07:31 +0200

    Cleaned up layout files.

--- myrkr/sections/05/music.py
+++ myrkr/sections/05/music.py
@@ -123,7 +123,7 @@ def make_layout(environment):
     spacing = baca.layout.Spacing(
         default=(1, 28),
     )
-    return baca.build.write_layout_ily(
+    return baca.section.make_layout_score(
         breaks,
         environment.metadata["time_signatures"],
         first_measure_number=environment.first_measure_number,
